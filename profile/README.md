f4062f04f841b9586b5a3eb2c5dfdff33afbc961
Tu Nguyen <tunguyen@Tus-MacBookPro.local>
2022-07-24 13:41:25 -0400

    fix the readme file & correct the img for project

--- profile/README.md
+++ profile/README.md
@@ -33,8 +33,8 @@ You can run a local copy of this site by issuing the following commands.
 $ git clone git@github.com:vi3t4lov3/My-Profile-React.git
 $ cd My-Profile-React
 $ Dont forget to run SOURCE schema.sql & seeds.sql in db folder
-$ run npm init 
-$ npm node .
+$ run npm i 
+$ npm start
 ```
 
 ## Usage
--- profile/src/assets/data/projects.js
+++ profile/src/assets/data/projects.js
@@ -8,7 +8,7 @@ const users = [
     {
         title: 'Stock Alert Tracker',
         description: 'We are creating an app that users can log in with their personal credentials. As an investor, I should be able to create alerts for specific stocks of my choice and add them to a list. I should be able to follow other alerts created by other users. These alerts will send a message or a text when a stock goes up or down to a particular price target specified by the user.',
-        img: project1,
+        img: project2,
         author: 'Tu Nguyen, Colin Brooks, Charnay Birton & Harris Ai',
         source: 'https://github.com/Danqest/15-Project2-Stock-Alert-Tracker',
         demo: 'https://lit-waters-98495.herokuapp.com/',
@@ -17,7 +17,7 @@ const users = [
     {
         title: 'AMDB - Actor/Movie Database',
         description: 'This project we has been created as a database to search actors/actresses or movies/tv show and be provided with a detailed description, trailer and movie information, and connections to GIFs that are correlated to the searched item to be shared with friends throughout social media.',
-        img: project2,
+        img: project1,
         author: 'Tu Nguyen, Victoria Hendricks, Corey Levine & Lauren Anderson',
         source: 'https://github.com/vi3t4lov3/GRP12-The-Movie-Finder',
         demo: 'https://vi3t4lov3.github.io/GRP12-The-Movie-Finder/',
